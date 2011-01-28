ba4ab1d2ca0ef2b357d7d5e33a4af224d22fb253
Hani Elabed <hani.elabed@gmail.com>
2011-01-27 22:07:52 -0600

    commented out some code in active scaffold that caused copying of the date_picker_bridge.js file for localization, and Heroku does not like anybody to touch the file system

--- vendor/plugins/active_scaffold/lib/active_scaffold/bridges/date_picker/bridge.rb
+++ vendor/plugins/active_scaffold/lib/active_scaffold/bridges/date_picker/bridge.rb
@@ -8,8 +8,9 @@ ActiveScaffold::Bridges.bridge "DatePicker" do
 
     if ActiveScaffold.js_framework == :jquery
       require File.join(directory, "lib/datepicker_bridge.rb")
-      FileUtils.cp(source, destination)
-      ActiveScaffold::Bridges::DatePickerBridge.localization(File.join(destination, 'date_picker_bridge.js'))
+      # commented out because Heroku does not like any file system writes
+      #FileUtils.cp(source, destination)
+      #ActiveScaffold::Bridges::DatePickerBridge.localization(File.join(destination, 'date_picker_bridge.js'))
     else
       # make sure that jquery files are removed
       FileUtils.rm(File.join(destination, 'date_picker_bridge.js')) if File.exist?(File.join(destination, 'date_picker_bridge.js'))
